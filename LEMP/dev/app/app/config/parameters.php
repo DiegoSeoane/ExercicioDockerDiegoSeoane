6d99fca3cb6336cbc56bcbbeb82e45d2218b1516
Diego Seoane <a22diegosg@iessanclemente.net>
2024-03-09 14:19:03 +0100

    initial commit

---
+++ LEMP/dev/app/app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'basededatos',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'user',
+    'database_password' => 'abc123.',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '8ED0yI0vtF5EUgRdDzy77GJnytQvL8FoRMFCPOwM5i32zVSIJPc76CgNovTmgM5h',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-03-09',
+    'locale' => 'es-ES',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'TAAi3obKnnE2xqxfgmnkVZEPM9wP152mtrTVGYJgKcGqz1AZ0ULo0fXxv8HMBiJr',
+    'cookie_iv' => 'NEvvh02q2Up3T4LInhNO4MvFeYaLCx99',
+    'new_cookie_key' => 'def00000904b2014041cfd7fe9cac09b69289350a0e6fadd1421fec253533aa57827e5bad74e3ec392f7b41175e2a1a389697dd20d441c7ad9f3040427f78a51e726d2d1',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwEuhkKtWVUIao53ZBZj1
+dlPU804rWih7tqG4Ps7LAR0K37OfxgbXH6W7MaYL4SFELCb9HHVbFPrlWflr+RIt
+7betPqiOJOLuAehR5+xlv/Se1JGxTKRwaZ6hw0WRMk3zJped1P0XwvK/uSF3kK4G
+MGyt/McX5rRVHFrYoaoOiI5vcj3IiJsBGjRlHi3Ab4JjetP2mHp0aGrISKpq54Px
+FFWGKeLJQy2q+WwHvGsgaYzMCE8l1OoQ3wmGcfYPgrmQ+jm6aBlQG6gP0wKDw/GC
+jVwILxyzWSqdHAl/r5+bEmjLpHm01+p4HUQDsTAwKPMEpd7llPcUJWNsLaipkn74
+8wIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDAS6GQq1ZVQhqj
+ndkFmPV2U9TzTitaKHu2obg+zssBHQrfs5/GBtcfpbsxpgvhIUQsJv0cdVsU+uVZ
++Wv5Ei3tt60+qI4k4u4B6FHn7GW/9J7UkbFMpHBpnqHDRZEyTfMml53U/RfC8r+5
+IXeQrgYwbK38xxfmtFUcWtihqg6Ijm9yPciImwEaNGUeLcBvgmN60/aYenRoashI
+qmrng/EUVYYp4slDLar5bAe8ayBpjMwITyXU6hDfCYZx9g+CuZD6ObpoGVAbqA/T
+AoPD8YKNXAgvHLNZKp0cCX+vn5sSaMukebTX6ngdRAOxMDAo8wSl3uWU9xQlY2wt
+qKmSfvjzAgMBAAECggEAAVnDu+KI7UsDhkAKiIIIHvmCZrf/4mhGqLiBzDJpL8vz
+pwJ/qEsCR4k3msnljhmqCN9bO9u143mEkyfkMpMpV1Ul/DrRfSMcLpUoM5L159DY
+FgY6rOToyWdPyjF32e4xahDvA0al0JQLGD4zhpBOt0ahO9s+rq/VTKkYU/q6HvOA
+kP/2qSZeH/W4CaO6hZaMu4guI5eLmGTmQz13rzalEqhcnQ3+J8tviwbcVvR4URpE
+vqO+9wvvnSmIK6m4X/FJWKHcRtjJoeZ94EjLum6ItS9jF7paGTHvDsyXp2+Hj3zv
+f1lRFCoe5rd9EEL1XurEEMpTlghY4XA7aMztTT44oQKBgQD8zAFn26WlYKjykbTK
+MePiA5eKRDBeRRVzsRoDESbRDYjYB49iCPaqYHB7IRjMYusQiRkW4wAVn63LPuY+
+uuyrnHxbugUoLyNX0eymC/h9wEaY4Nk+1mMuhzRdC9CsTcInTfkrriMYldB5cDig
+KwFmcr9MW/xvbhxXX/lp9OY4kwKBgQDCu2CwpU7yIR+ozNyShBuPMoaY/1nIHDuc
+QZA+z2Fws1BvXMMe2HYDC5M4rpbCRxIoMms81yxgNSPutfDtz7LmeRpfBQ2YT4r8
+Ycht/EhEOwUNG9zeWl2GMFDTc1j7s4OWjepxvXLas+cJEMxjZK1mU8ScyheV5TKJ
+zTgtphpaIQKBgQDzSOXFbNZCjP6qy10BBfh4x9osP6mI39VgTL1q3DPOFsdul//+
+af/xQfSeGuEBueTdOgPCSd4H8W9RXYw6LKyeD3CC2kgiPxM1bhrgRj7zznpioGmS
+lsJGwem77iX53K+QIfM9fU3bWDqU1FI6moizbpT5yMDtADwYPo4K0ZxKLwKBgHHR
+FU2KAV+SLq/5tT5JytXioTsRnMdSgSDuTUH48inuFKhSXpVLzYYwIMdzfBhnc9xc
+w8MWvRAqXysp2n0U9aqxw9Ef4fdRHpovCPgICbEfke88htbLmHhTCxrcA3lyTgoU
+PqBmK5haqEHrOJsrgBIcCgHeOPTatrRGZ3x+5k4hAoGABzjnbhVoF3++tBiar+11
+bHdcRzaG/yeQqOkEOi0p7V1NY15jcMUjztPa4r/oi9n3FzHgPHsC0IN63foz6sZs
+b971TIy3vuaDOBWD/7If4QZi3nSb830zCERdfTrtFJnRCK5xknXRIaRkEC2VZsi0
+O6VfSH1zuVI1MxQ3vLL/mhE=
+-----END PRIVATE KEY-----
+',
+  ),
+);
